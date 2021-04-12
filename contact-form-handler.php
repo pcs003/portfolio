b5b24b3336cfb4e52c0f1f0daa7a8b7ff3b7fbdb
pcs003 <50216227+pcs003@users.noreply.github.com>
2021-04-12 19:13:03 -0400

    Add files via upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,23 +1,10 @@
-<?php
-    $name = $_Post['name'];
-    $visitor_email = $_Post['email'];
-    $message = $_Post['message'];
-
-    $email_from = $visitor_email;
-
-    $email_subject = 'New Form Submission';
-
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $email.\n";
-
-    $to = 'parthshahportfolio@gmail.com';
-
-    $headers = "From: $email_from \r\n";
-
-    $header .= "Reply-To: $visitor_email \r\n";
-
-    mail($to, $email_subject, $email_body, $headers)
-
-    header("Location: index.html");
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
